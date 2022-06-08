591294136ec0afb0b328edd8dcb0c3e161f750ed
Mike Heavers <mheavers@gmail.com>
2022-06-08 12:21:19 -0700

    solved 2-1

--- depth2.js
+++ depth2.js
@@ -1,16 +1,60 @@
-import depthArray from "./depthArray.js";
-const depths = depthArray();
-let curDepthIndex = 0;
-const numDepths = depths.length;
-console.log(numDepths);
-let curDepthSum;
-let depthIncreaseCount = 0;
+let input = document.querySelector("input");
+let textarea = document.querySelector("textarea");
+const courseArray = [];
+let depth = 0,
+  position = 0;
 
-for (let i = 0; i < numDepths - 2; i++) {
-  const depthSum = depths[i] + depths[i + 1] + depths[i + 2];
-  console.log(curDepthSum, depthSum, depthIncreaseCount);
-  if (curDepthSum && depthSum > curDepthSum) {
-    depthIncreaseCount++;
+input.addEventListener("change", () => {
+  let files = input.files;
+
+  if (files.length == 0) return;
+
+  const file = files[0];
+
+  let reader = new FileReader();
+
+  reader.onload = (e) => {
+    const file = e.target.result;
+
+    const lines = file.split(/\r\n|\n/);
+    //console.log(lines);
+    lines.forEach((line) => {
+      const lineArr = line.split(" ");
+      const lineObj = {
+        direction: lineArr[0],
+        amt: parseInt(lineArr[1]),
+      };
+      //console.log(lineObj);
+      courseArray.push(lineObj);
+    });
+    textarea.value = lines.join("\n");
+    chartCourse();
+  };
+
+  reader.onerror = (e) => alert(e.target.error.name);
+
+  reader.readAsText(file);
+});
+
+function chartCourse() {
+  for (let i = 0; i < courseArray.length; i++) {
+    const curMovement = courseArray[i];
+    const { direction, amt } = curMovement;
+    switch (direction) {
+      case "forward":
+        position += amt;
+        break;
+      case "up":
+        depth -= amt;
+        break;
+      case "down":
+        depth += amt;
+        break;
+      default:
+        console.log("unhandled direction value", direction);
+        break;
+    }
   }
-  curDepthSum = depthSum;
+  console.log(depth * position); //answer
+  //on to https://adventofcode.com/2021/day/2#part2
 }
